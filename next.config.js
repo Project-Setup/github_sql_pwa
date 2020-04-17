1e2e826996c932a25f72178958c09fc975426be8
clfhhc <clfhhc@gmail.com>
2020-04-17 11:03:02 -0700

    changed sw path

--- next.config.js
+++ next.config.js
@@ -1,25 +1,29 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const withPWA = require('next-pwa');
+const path = require('path');
 const envMapping = require('./configs/env.mapping');
 
 const isProd = process.env.NODE_ENV === 'production';
+const { LINK_PREFIX } = envMapping;
 
 module.exports = () =>
   withPWA({
     env: envMapping,
     target: 'serverless',
     poweredByHeader: false,
-    assetPrefix: envMapping.LINK_PREFIX,
+    assetPrefix: LINK_PREFIX,
 
     pwa: {
       disable: !isProd,
-      scope: `${envMapping.LINK_PREFIX}/`,
-      // sw: `${envMapping.LINK_PREFIX}/sw.js`,
+      scope: `${LINK_PREFIX}/`,
+      sw: `${LINK_PREFIX}/sw.js`,
       dest: 'public',
+      swDest: path.join(__dirname, `/public/sw.js`),
       navigationPreload: true,
-      modifyURLPrefix: {
-        app: envMapping.LINK_PREFIX,
-      },
+      // globPatterns: ['app/static/**/*'],
+      // modifyURLPrefix: {
+      //   app: LINK_PREFIX,
+      // },
       // publicExcludes: [],
       // exclude: ['**/node_modules/**/*'],
     },
