5bd05813f24cead7e3fd6f5883bc4fe4e1fea2d6
clfhhc <clfhhc@gmail.com>
2020-04-22 17:01:20 -0700

    add manifest

--- configs/env.mapping.js
+++ configs/env.mapping.js
@@ -3,17 +3,17 @@ const path = require('path');
 require('dotenv').config({
   path: path.join(__dirname, `../.env/${process.env.NODE_ENV}.env`),
 });
-const packageLockJson = require('../package-lock.json');
 
 // env mapping for exposure to client
 module.exports = {
   TEST_CLIENT: process.env.test,
   TEST_PAGE_VAR: process.env.TEST_PAGE_VAR,
   ENV: process.env.NODE_ENV,
-  LINK_PREFIX: process.env.LINK_PREFIX || '',
-  THEME_COLOR: process.env.THEME_COLOR,
-  MANIEFST_PATH: process.env.MANIEFST_PATH,
-  ICON_192_PATH: process.env.ICON_192_PATH,
-  FAV_ICON_PATH: process.env.FAV_ICON_PATH,
-  SQL_JS_VERSION: packageLockJson.dependencies['sql.js'].version,
+  FOLDER: process.env.FOLDER || '',
+  LINK_PREFIX: `/${process.env.FOLDER}` || '',
+  THEME_COLOR: process.env.THEME_COLOR || '',
+  MANIEFST_PATH: process.env.MANIEFST_PATH || '',
+  ICON_192_PATH: process.env.ICON_192_PATH || '',
+  ICON_512_PATH: process.env.ICON_512_PATH || '',
+  FAV_ICON_PATH: process.env.FAV_ICON_PATH || '',
 };
--- next.config.js
+++ next.config.js
@@ -1,12 +1,19 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const withPWA = require('next-pwa');
+const withManifest = require('next-manifest');
 const withTM = require('next-transpile-modules')(['typeorm/browser']);
 const webpack = require('webpack');
 const defaultCache = require('next-pwa/cache');
 const envMapping = require('./configs/env.mapping');
 
 const isProd = process.env.NODE_ENV === 'production';
-const { LINK_PREFIX } = envMapping;
+const {
+  LINK_PREFIX,
+  FOLDER,
+  THEME_COLOR,
+  ICON_192_PATH,
+  ICON_512_PATH,
+} = envMapping;
 
 // tranfrom precache url for browsers that encode dynamic routes
 // i.e. "[id].js" => "%5Bid%5D.js"
@@ -19,69 +26,99 @@ const encodeUriTransform = async (manifestEntries) => {
 };
 
 module.exports = () =>
-  withPWA(
-    withTM({
-      webpack: (config, { isServer }) => {
-        config.plugins.push(
-          new webpack.ProvidePlugin({
-            'window.SQL': 'sql.js/dist/sql-wasm.js',
-          })
-        );
-        if (!isServer) {
-          config.node = {
-            fs: 'empty',
-            net: 'empty',
-            tls: 'empty',
-          };
-        }
-        return config;
-      },
+  withManifest(
+    withPWA(
+      withTM({
+        // sql.js
+        webpack: (config, { isServer }) => {
+          config.plugins.push(
+            new webpack.ProvidePlugin({
+              'window.SQL': 'sql.js/dist/sql-wasm.js',
+            })
+          );
+          if (!isServer) {
+            config.node = {
+              fs: 'empty',
+              net: 'empty',
+              tls: 'empty',
+            };
+          }
+          return config;
+        },
 
-      env: envMapping,
-      target: 'serverless',
-      poweredByHeader: false,
-      assetPrefix: LINK_PREFIX,
+        env: envMapping,
+        target: 'serverless',
+        poweredByHeader: false,
+        assetPrefix: LINK_PREFIX,
 
-      pwa: {
-        disable: !isProd,
-        subdomainPrefix: LINK_PREFIX,
-        dest: 'public',
-        manifestTransforms: [encodeUriTransform],
-        runtimeCaching: [
-          ...defaultCache,
-          {
-            urlPattern: /^https?.*/,
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'offlineCache',
-              networkTimeoutSeconds: 15,
-              expiration: {
-                maxEntries: 150,
-                maxAgeSeconds: 30 * 24 * 60 * 60,
-              },
-              cacheableResponse: {
-                statuses: [0, 200],
+        // service worker
+        pwa: {
+          disable: !isProd,
+          subdomainPrefix: LINK_PREFIX,
+          dest: 'public',
+          manifestTransforms: [encodeUriTransform],
+          runtimeCaching: [
+            ...defaultCache,
+            {
+              urlPattern: /^https?.*/,
+              handler: 'NetworkFirst',
+              options: {
+                cacheName: 'offlineCache',
+                networkTimeoutSeconds: 15,
+                expiration: {
+                  maxEntries: 150,
+                  maxAgeSeconds: 30 * 24 * 60 * 60,
+                },
+                cacheableResponse: {
+                  statuses: [0, 200],
+                },
               },
             },
-          },
-          {
-            urlPattern: ({ event }) => event.request.mode === 'navigate',
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'offlineCache',
-              expiration: {
-                maxEntries: 150,
-                maxAgeSeconds: 30 * 24 * 60 * 60,
-              },
-              cacheableResponse: {
-                statuses: [0, 200],
+            {
+              urlPattern: ({ event }) => event.request.mode === 'navigate',
+              handler: 'CacheFirst',
+              options: {
+                cacheName: 'offlineCache',
+                expiration: {
+                  maxEntries: 150,
+                  maxAgeSeconds: 30 * 24 * 60 * 60,
+                },
+                cacheableResponse: {
+                  statuses: [0, 200],
+                },
               },
             },
-          },
-        ],
-        navigationPreload: true,
-        // publicExcludes: [],
-        // exclude: ['**/node_modules/**/*'],
-      },
-    })
+          ],
+          navigationPreload: true,
+          // publicExcludes: [],
+          // exclude: ['**/node_modules/**/*'],
+        },
+
+        // manifest
+        manifest: {
+          /* eslint-disable @typescript-eslint/camelcase */
+          output: 'public',
+          short_name: FOLDER,
+          name: FOLDER,
+          start_url: `${LINK_PREFIX}/`,
+          background_color: THEME_COLOR,
+          display: 'standalone',
+          scope: `${LINK_PREFIX}/`,
+          dir: 'ltr', // text direction: left to right
+          theme_color: THEME_COLOR,
+          icons: [
+            {
+              src: `${LINK_PREFIX}${ICON_192_PATH}`,
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: `${LINK_PREFIX}${ICON_512_PATH}`,
+              sizes: '512x512',
+              type: 'image/png',
+            },
+          ],
+        },
+      })
+    )
   );
